c21e768bfb2584f60f256c3c55be342827956727
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-04-10 14:56:37 +0200

    setup pdoc script

--- doc/py/gen.py
+++ doc/py/gen.py
@@ -14,13 +14,13 @@ cmod.doc["Infimum"] = pdoc.Variable("Infimum", cmod, "represents a `clingo.Symbo
 cmod.doc["Supremum"] = pdoc.Variable("Supremum", cmod, "represents a `clingo.Symbol` of type `clingo.SymbolType.Supremum`")
 pdoc.link_inheritance(ctx)
 
-def replace(s):
-    s = s.replace('href="clingo.html', 'href="clingo/')
-    s = s.replace('href="../clingo.html', 'href="../')
-    s = s.replace('href="clingo/ast.html', 'href="ast/')
-    s = re.sub(r"['\"]https://cdnjs\.cloudflare\.com/.*/([^/'\"]+\.(css|js))['\"]", r"'\2/\1'", s)
+def replace(s, root):
+    s = s.replace('href="clingo.html', 'href="clingo/index.html')
+    s = s.replace('href="../clingo.html', 'href="../index.html')
+    s = s.replace('href="clingo/ast.html', 'href="ast/index.html')
+    s = re.sub(r"['\"]https://cdnjs\.cloudflare\.com/.*/([^/'\"]+\.(css|js))['\"]", r"'{}\2/\1'".format(root), s)
     return s
 
 os.makedirs("clingo/ast", exist_ok=True)
-open("clingo/index.html", "w").write(replace(cmod.html(external_links=True)))
-open("clingo/ast/index.html", "w").write(replace(amod.html(external_links=True)))
+open("clingo/index.html", "w").write(replace(cmod.html(external_links=True), "../"))
+open("clingo/ast/index.html", "w").write(replace(amod.html(external_links=True), "../../"))
