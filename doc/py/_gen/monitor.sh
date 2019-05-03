f5f60cae2fe8c07187db6220923ea8b12e101b5e
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-05-03 14:57:34 +0200

    fix monitor script

--- doc/py/_gen/monitor.sh
+++ doc/py/_gen/monitor.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 cd "$(dirname "$0")/.."
 jekyll serve -l &
 trap "kill $!" EXIT
 cd _gen
-while inotifywait -e DELETE_SELF templates/html.mako ../../../build/*/bin/python/clingo*.so; do
-    sleep 1
-    python3 gen.py
+
+inotifywait -rme ATTRIB --format "%f" templates ../../../build/release/bin/python | while read file; do
+    case "$file" in
+        *.mako)
+            ;&
+        *.so)
+            sleep 1
+            python3 gen.py
+            ;;
+    esac
 done
