eda0ec2c6d78cbbdab04e09aee8e77d5f9d1ab9e
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-05-06 19:07:34 +0200

    document all control methods

--- libpyclingo/pyclingo.cc
+++ libpyclingo/pyclingo.cc
@@ -7620,107 +7620,191 @@ class GroundProgramObserver:
 ```
 )"},
     {"register_propagator", to_function<&ControlWrap::registerPropagator>(), METH_O,
-R"(register_propagator(self, propagator) -> None
+R"(register_propagator(self, propagator: Propagator) -> None
 
 Registers the given propagator with all solvers.
 
 Parameters
 ----------
-propagator -- the propagator to register
+propagator : Propagator
+    The propagator to register.
 
+Returns
+-------
+None
+
+Notes
+-----
 A propagator should be a class of the form below. Not all functions have to be
 implemented and can be omitted if not needed.
 
-class Propagator(object)
-    init(self, init) -> None
-        This function is called once before each solving step.  It is used to
-        map relevant program literals to solver literals, add watches for
-        solver literals, and initialize the data structures used during
-        propagation.
+```python
+class Propagator:
+    def init(self, init: PropagateInit) -> None:
+        """
+        This function is called once before each solving step.
 
-        Arguments:
-        init -- PropagateInit object
+        It is used to map relevant program literals to solver literals, add
+        watches for solver literals, and initialize the data structures used
+        during propagation.
 
-        Note that this is the last point to access theory atoms.  Once the
-        search has started, they are no longer accessible.
+        Parameters
+        ----------
+        init : PropagateInit
+            Object to initialize the propagator.
 
-    propagate(self, control, changes) -> None
+        Returns
+        -------
+        None
+
+        Notes
+        -----
+        This is the last point to access theory atoms.  Once the search has
+        started, they are no longer accessible.
+        """
+
+    def propagate(self, control: PropagateControl, changes: List[int]) -> None:
+        """
         Can be used to propagate solver literals given a partial assignment.
 
-        Arguments:
-        control -- PropagateControl object
-        changes -- list of watched solver literals assigned to true
+        Parameters
+        ----------
+        control : PropagateControl
+            Object to control propagation.
+        changes : List[int]
+            List of watched solver literals assigned to true.
 
-        Usage:
+        Returns
+        -------
+        None
+
+        Notes
+        -----
         Called during propagation with a non-empty list of watched solver
         literals that have been assigned to true since the last call to either
-        propagate, undo, (or the start of the search) - the change set.  Only
-        watched solver literals are contained in the change set.  Each literal
+        propagate, undo, (or the start of the search) - the change set. Only
+        watched solver literals are contained in the change set. Each literal
         in the change set is true w.r.t. the current Assignment.
-        PropagateControl.add_clause can be used to add clauses.  If a clause is
-        unit resulting, it can be propagated using
-        PropagateControl.propagate().  If either of the two methods returns
+        `PropagateControl.add_clause` can be used to add clauses. If a clause
+        is unit resulting, it can be propagated using
+        `PropagateControl.propagate`. If either of the two methods returns
         False, the propagate function must return immediately.
 
-          c = ...
-          if not control.add_clause(c) or not control.propagate(c):
-              return
+        ```python
+        c = ...
+        if not control.add_clause(c) or not control.propagate(c):
+            return
+        ```
 
         Note that this function can be called from different solving threads.
         Each thread has its own assignment and id, which can be obtained using
-        PropagateControl.id().
+        `PropagateControl.id`.
+        """
 
-    undo(self, thread_id, assignment, changes) -> None
+    def undo(self, thread_id: int, assignment: Assignment,
+             changes: List[int]) -> None:
+        """
         Called whenever a solver with the given id undos assignments to watched
         solver literals.
 
-        Arguments:
-        thread_id  -- the solver thread id
-        assignment -- the assignment of the solver
-        changes    -- list of watched solver literals whose assignment is undone
+        Parameters
+        ----------
+        thread_id : int
+            The solver thread id.
+        assignment : Assignment
+            Object for inspecting the partial assignment of the solver.
+        changes : List[int]
+            The list of watched solver literals whose assignment is undone.
 
+        Returns
+        -------
+        None
+
+        Notes
+        -----
         This function is meant to update assignment dependent state in a
-        propagator.
+        propagator but not to modify the current state of the solver.
+        """
 
-    check(self, control) -> None
+    def check(self, control: PropagateControl) -> None:
+        """
         This function is similar to propagate but is called without a change
-        set on propagation fixpoints.  When exactly this function is called,
-        can be configured using the @ref PropagateInit.check_mode property.
+        set on propagation fixpoints.
 
-        Note that this function is called even if no watches have been added.
+        When exactly this function is called, can be configured using the @ref
+        PropagateInit.check_mode property.
 
-        Arguments:
-        control -- PropagateControl object
+        Parameters
+        ----------
+        control : PropagateControl
+            Object to control propagation.
 
+        Returns
+        -------
+        None
+
+        Notes
+        -----
         This function is called even if no watches have been added.
+        """
 
-    decide(self, thread_id, assignment, fallback) -> int
+    def decide(self, thread_id: int, assignment: Assignment, fallback: int) -> int:
+        """
         This function allows a propagator to implement domain-specific
-        heuristics. It is called whenever propagation reaches a fixed point.
+        heuristics.
 
-        Arguments:
-        thread_id  -- the solver thread id
-        assignment -- the assignment of the solver
-        fallback   -- the literal choosen by the solver's heuristic
+        It is called whenever propagation reaches a fixed point.
 
+        Parameters
+        ----------
+        thread_id : int
+            The solver thread id.
+        assignment : Assignment
+            Object for inspecting the partial assignment of the solver.
+        fallback : int
+            The literal choosen by the solver's heuristic.
+
+        Returns
+        -------
+        int
+            Тhe next solver literal to make true.
+
+        Notes
+        -----
         This function should return a free solver literal that is to be
-        assigned true.  In case multiple propagators are registered, this
+        assigned true. In case multiple propagators are registered, this
         function can return 0 to let a propagator registered later make a
-        decision.  If all propagators return 0, then the fallback literal is
-        used.)"},
+        decision. If all propagators return 0, then the fallback literal is
+        used.
+        """
+```
+        )"},
     {"interrupt", to_function<&ControlWrap::interrupt>(), METH_NOARGS,
 R"(interrupt(self) -> None
 
 Interrupt the active solve call.
 
-This function is thread-safe and can be called from a signal handler. If no
-search is active the subsequent call to solve() is interrupted. The SolveResult
-of the above solving methods can be used to query if the search was
-interrupted.)"},
-    {"backend", to_function<&ControlWrap::backend>(), METH_NOARGS,
-R"(backend() -> Backend
+Returns
+-------
+None
 
-Returns a Backend object providing a low level interface to extend a logic program.)"},
+Notes
+-----
+This function is thread-safe and can be called from a signal handler. If no
+search is active, the subsequent call to `Control.solve` is interrupted. The
+result of the `Control.solve` method can be used to query if the search was
+interrupted.
+)"},
+    {"backend", to_function<&ControlWrap::backend>(), METH_NOARGS,
+R"(backend(self) -> Backend
+
+Returns a `Backend` object providing a low level interface to extend a logic
+program.
+
+Returns
+-------
+Backend
+)"},
     {nullptr, nullptr, 0, nullptr}
 };
 
