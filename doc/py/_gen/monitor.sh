6e1436d8abc9fb3efd94ccf4e4e7a00e1baf5fb8
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2019-05-24 13:58:33 +0200

    refine monitor script

--- doc/py/_gen/monitor.sh
+++ doc/py/_gen/monitor.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
+
+export cpucount=$[ $(cat /proc/cpuinfo | grep processor | wc -l) - 1 ]
+
+(
+    cd _gen
+    make -j${cpucount} -C../../../build/release pyclingo
+    python3 gen.py
+)
+
 export BUNDLE_PATH=$(pwd)/.gem
 bundle install
 bundle exec jekyll serve -l &
 trap "kill $!" EXIT
 cd _gen
 
-inotifywait -rme ATTRIB --format "%f" . ../../../build/release/bin/python | while read file; do
+
+inotifywait -rme ATTRIB --format "%f" . ../../../build/release/bin/python ../../../libpyclingo | while read file; do
     case "$file" in
         *.py)
             ;&
@@ -16,6 +26,11 @@ inotifywait -rme ATTRIB --format "%f" . ../../../build/release/bin/python | whil
         *.so)
             sleep 1
             python3 gen.py
+            ;;
+        *.h)
+            ;&
+        *.cc)
+            make -j${cpucount} -C../../../build/release pyclingo
             ;;
     esac
 done
